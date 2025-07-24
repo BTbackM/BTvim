c2e2ae3ea5dd790513cb86197aa9cc87a0662f23
BTbackM <felix.blanco@utec.edu.pe>
2025-07-24 09:03:16 -0500

    fix: Configured Nvim-treesitter and added TS languages support

--- lua/bt/plugins/nvim-treesitter.lua
+++ lua/bt/plugins/nvim-treesitter.lua
@@ -3,8 +3,26 @@ return {
 	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"nvim-treesitter/playground",
 	},
+	config = function()
+		require "nvim-treesitter.configs".setup({
+			ensure_installed = {
+				"lua",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = {},
+			},
+			auto_install = false,
+		})
+	end
 }
