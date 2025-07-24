d48c487f2b291ce5e80c1318c120cc14101be866
BTbackM <felix.blanco@utec.edu.pe>
2025-07-24 13:03:37 -0500

    feat: Added and configured Supermaven plugin

---
+++ lua/bt/plugins/supermaven.lua
@@ -0,0 +1,14 @@
+local opts = {
+	keymaps = {
+		accept_suggestion = "<C-l>",
+		clear_suggestion = "<C-h>",
+		accept_word = "<C-k>",
+	}
+}
+
+return {
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require "supermaven-nvim".setup(opts)
+	end,
+}
