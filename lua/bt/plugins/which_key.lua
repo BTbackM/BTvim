c3d938a65affe61f0ee22412f6f9be3eb13616be
BTbackM <felix.blanco@utec.edu.pe>
2025-08-01 16:32:57 -0500

    feat: Added new opts to Which-key, icons and keys

--- lua/bt/plugins/which_key.lua
+++ lua/bt/plugins/which_key.lua
@@ -1,6 +1,42 @@
-if false then return {} end
+if false then
+	return {}
+end
 
 local opts = {
+	delay = 0,
+	icons = {
+		mappings = vim.g.have_nerd_font,
+		keys = vim.g.have_nerd_font and {} or {
+			Up = "<Up> ",
+			Down = "<Down> ",
+			Left = "<Left> ",
+			Right = "<Right> ",
+			C = "<C-…> ",
+			M = "<M-…> ",
+			D = "<D-…> ",
+			S = "<S-…> ",
+			CR = "<CR> ",
+			Esc = "<Esc> ",
+			ScrollWheelDown = "<ScrollWheelDown> ",
+			ScrollWheelUp = "<ScrollWheelUp> ",
+			NL = "<NL> ",
+			BS = "<BS> ",
+			Space = "<Space> ",
+			Tab = "<Tab> ",
+			F1 = "<F1>",
+			F2 = "<F2>",
+			F3 = "<F3>",
+			F4 = "<F4>",
+			F5 = "<F5>",
+			F6 = "<F6>",
+			F7 = "<F7>",
+			F8 = "<F8>",
+			F9 = "<F9>",
+			F10 = "<F10>",
+			F11 = "<F11>",
+			F12 = "<F12>",
+		},
+	},
 	spec = {
 		{
 			mode = { "n", "v" },
@@ -40,7 +76,7 @@ local kmaps = {
 
 return {
 	"folke/which-key.nvim",
-	event = "VeryLazy",
+	event = "VimEnter",
 	opts = opts,
 	keys = kmaps,
 }
