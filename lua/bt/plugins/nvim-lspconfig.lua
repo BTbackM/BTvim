f93f52f1fc125d9057f201e8c8b69c9423b04bb6
BTbackM <felix.blanco@utec.edu.pe>
2025-08-04 14:09:11 -0500

    feat: Now using basic nvim-lspconfig config and added treesitter support for more languages

--- lua/bt/plugins/nvim-lspconfig.lua
+++ lua/bt/plugins/nvim-lspconfig.lua
@@ -1,5 +1,12 @@
-if false then
-	return {}
+if true then
+	return {
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"j-hui/fidget.nvim",
+			"saghen/blink.cmp",
+		},
+	}
 end
 
 return {
@@ -41,36 +48,6 @@ return {
 				end
 
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if
-					client
-					and client_supports_method(
-						client,
-						vim.lsp.protocol.Methods.textDocument_documentHighlight,
-						event.buf
-					)
-				then
-					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
-
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
-
-					vim.api.nvim_create_autocmd("LspDetach", {
-						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-						end,
-					})
-				end
-
 				if
 					client
 					and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
@@ -115,33 +92,9 @@ return {
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		local servers = {
-			lua_ls = {
-				cmd = { "lua-language-server" },
-				filetypes = { "lua" },
-				root_markers = {
-					".luarc.json",
-					".luarc.jsonc",
-					".luacheckrc",
-					".stylua.toml",
-					"stylua.toml",
-					"selene.toml",
-					"selene.yml",
-					".git",
-				},
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						runtime = {
-							version = "LuaJIT",
-						},
-					},
-				},
-			},
+			lua_ls = require("bt.lsp.lua-ls"),
+			gopls = require("bt.lsp.gopls"),
+			omnisharp = require("bt.lsp.omnisharp"),
 		}
 
 		local ensure_installed = vim.tbl_keys(servers or {})
